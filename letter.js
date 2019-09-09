b0cb63c7566963adb8213534207dc6438f51e958
Zev Hoffman <zevubu@gmail.com>
2019-09-09 12:03:09 -0700

    started working on letter.js added if else statments to determine if you got it right

--- letter.js
+++ letter.js
@@ -1,5 +1,28 @@
 let Letter = function(value){
+    this.letter = value;
+    this.guessed = false;
+    this.toString = function(){
+
+        if(this.letter === " "){
+            this.guessed = true;
+            return " ";
+        }else {
+            if (this.guessed === false){
+                return "_";
+            } else {
+                return this.letter;
+            }
+        };
+    };
+
+    this.guess = function (guess) {
+        if(guess === this.letter){
+            this.guessed = true;
+        }
+    }
 
 }
+
+// Letter("hello")
 
 module.exports = Letter;
