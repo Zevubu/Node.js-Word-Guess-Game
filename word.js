0c52553d5b9fd6fafed7430633008b4b1b535b73
Zev Hoffman <zevubu@gmail.com>
2019-09-09 12:09:43 -0700

    started working on word.js to push to letter.js and check if it's there.

--- word.js
+++ word.js
@@ -1,6 +1,13 @@
 let Letter = require("./letter");
 
 let Word = function(answer){
+    // this answer letter array
+    this.letArray = [];
+
+    for(let i = 0; i < answer.length; i++){
+        let letter = new Letter(answer[i]);
+        this.letArray.push(letter);
+    }
 
 }
 
