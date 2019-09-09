4bf643021c447a21794f87b7ed43704a925dd52d
Zev Hoffman <zevubu@gmail.com>
2019-09-09 12:29:39 -0700

    added my assumed vairiables

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 let Word = require("./word")
 let inquirer = require("inquirer")
 
+// array of alphabet
+let alphaString = "abcdefghijklmnopqrstuvwxyz"
+
+// Variables
+let randomIndexer;
+let randomWord;
+let chosenWord;
+
+let requireNewWord = false;
+
+let incorrectLetters = [];
+let correctLetters = [];
+
+let guessesLeft = 10;
+
+
+
+
+
+
 // list of words.
 // I may want to change this to and editable addable folder at a later date.
 
 // puts random word into Word file.
+
 
 // function 
 
@@ -12,6 +33,6 @@ let inquirer = require("inquirer")
 
     // if statment to intiate inquirer.
 
-    
+
 // function restart game.
 
