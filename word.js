37739e6ea313c4e75da35b861a5bfcf72983b253
Zev Hoffman <zevubu@gmail.com>
2019-09-09 12:19:50 -0700

    expanded word.js

--- word.js
+++ word.js
@@ -2,11 +2,25 @@ let Letter = require("./letter");
 
 let Word = function(answer){
     // this answer letter array
-    this.letArray = [];
+    this.lettArray = [];
 
     for(let i = 0; i < answer.length; i++){
         let letter = new Letter(answer[i]);
-        this.letArray.push(letter);
+        this.lettArray.push(letter);
+    }
+
+    this.log = function(){
+        answerLog = "";
+        for (let l = 0; l < this.lettArray; l++){
+            answerLog += this.lettArray[l] + " ";
+        }
+        console.log(answerLog + "\n")
+    }
+
+    this.userGuess = function(input){
+        for(let j = 0; j < this.lettArray.length; j++){
+            this.lettArray[j].guess(input);
+        }
     }
 
 }
